0b76b6d37b6655b218a9959ff701549018defb20
Karpov Yuri <yurick@bk.ru>
2022-06-18 16:02:36 +0300

    fix: Исправить адаптивность

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import data from '../utils/constants';
 import Card from './card.jsx'
 
 const Cards = () => {
+
+  useEffect(() => {
+    if (data.length <= 3) {
+      const height = document.querySelector('.root');
+      height.classList.add('root_small');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+
 
   const elements = data.map((item) => {
 
